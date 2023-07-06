8f4a6fb80c024a2ce5753364093973ca6f3da90b
minhtri06 <pmtri.tvv@outlook.com>
2023-07-06 19:30:22 +0700

    feat: register user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,6 +9,22 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users,email',
+            'password' => 'required|string',
+        ]);
+        $fields['password'] = bcrypt($fields['password']);
+
+        $user = User::create($fields);
+
+        $token = $user->createToken('myAppToken', [$user->role])->plainTextToken;
+
+        return response(['data' => ['user' => $user, 'token' => $token]], 201);
+    }
+
     public function login(Request $request)
     {
         $request->validate([
--- routes/api/auth.php
+++ routes/api/auth.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 Route::middleware('auth:sanctum')
     ->post('/logout', [AuthController::class, 'logout']);
